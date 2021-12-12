a7f682ee786e2bbe6e9bb6640306accbc3e9a843
Corina Chen <cchen32@Corinas-MacBook-Pro.local>
2021-12-12 15:12:34 -0500

    added skeleton

---
+++ OrderedArrayList.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class OrderedArrayList
+{
+
+  // instance of class ArrayList, holding objects of type Integer
+  // (i.e., objects of a class that implements interface Integer)
+  private ArrayList<Integer> _data;
+
+  // default constructor
+  // initializes instance variables
+  public OrderedArrayList()
+  {
+
+  }
+
+  public String toString()
+  {
+
+  }
+
+  public Integer remove( int i )
+  {
+
+  }
+
+  public int size()
+  {
+
+  }
+
+  public Integer get( int i )
+  {
+
+  }
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a linear search to find appropriate index
+  public void addLinear(Integer newVal)
+  {
+
+  }
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a binary search to find appropriate index
+  public void addBinary(Integer newVal)
+  {
+
+  }
+
+  // main method solely for testing purposes
+  public static void main( String[] args )
+  {
+    /*-----v-------move-me-down-----------------v--------
+    OrderedArrayList Franz = new OrderedArrayList();
+    // testing linear search
+    for( int i = 0; i < 15; i++ )
+      Franz.addLinear( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+    // testing binary search
+    Franz = new OrderedArrayList();
+    for( int i = 0; i < 15; i++ )
+      Franz.addBinary( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+      =====^====================================^=========*/
+
+  }//end main()
+
+}//end class OrderedArrayList
