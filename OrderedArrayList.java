d3dcc481d218ebccae9ba10608099a2590c14454
nadahameed <90862244+nadahameed@users.noreply.github.com>
2021-12-12 20:38:39 -0500

    Update OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
-public class OrderedArrayList {
+public class OrderedArrayList
+{
 
   // instance of class ArrayList, holding objects of type Integer
   // (i.e., objects of a class that implements interface Integer)
@@ -8,23 +9,28 @@ public class OrderedArrayList {
 
   // default constructor
   // initializes instance variables
-  public OrderedArrayList() {
-    _data = new ArrayList<Integer> (10);
+  public OrderedArrayList()
+  {
+    _data = new ArrayList<Integer> (23);
   }
 
-  public String toString() {
+  public String toString()
+  {
     return _data.toString();
   }
 
-  public Integer remove( int i ) {
+  public Integer remove( int i )
+  {
     return _data.remove(i);
   }
 
-  public int size() {
+  public int size()
+  {
     return _data.size();
   }
 
-  public Integer get( int i ) {
+  public Integer get( int i )
+  {
     return _data.get(i);
   }
 
@@ -33,65 +39,85 @@ public class OrderedArrayList {
   // uses a linear search to find appropriate index
   public void addLinear(Integer newVal)
   {
+    boolean inserted = false;
     if (size() == 0){
       _data.add(newVal);
-    }
-    else {
+    } else {
       for (int i = 0; i < size(); i++){
         if (newVal <= _data.get(i)){
           _data.add(i, newVal);
-          return;
+          inserted = true;
+          break;
+          //return; can include instead of using boolean inserted
         }
       }
+      if (inserted == false){
       _data.add(newVal);
+    }
     }
   }
 
   // inserts newVal at the appropriate index
   // maintains ascending order of elements
   // uses a binary search to find appropriate index
-  public void addBinary(Integer newVal) {
+public void addBinary(Integer newVal) {
+  if (size() == 0){
+    _data.add(newVal);
+  }
+  else {
     int left = 0;
     int right = size() - 1;
-    if ( size() == 0 ) {
-      _data.add(newVal);
-    }
-    else {
-      while (left < right ) {
-        int mid = ( left + right ) / 2;
-        if (_data.get(mid) == newVal) {
+
+    while (true) {
+
+      int mid = (int)((left + right) / 2);
+      int midVal = _data.get(mid);
+
+      if(midVal == newVal) {
+        _data.add(mid, newVal);
+        break;
+      }
+      else if((right - left) < 1){
+        if (newVal > midVal){
+          _data.add(mid + 1, newVal);
+          break;
+        } else{
           _data.add(mid, newVal);
           break;
         }
-        else if (_data.get(mid) < newVal ) {
-          left = mid + 1;
-        }
-        else {
-          right = mid - 1;
-        }
       }
-      _data.add(newVal);
+      else if(newVal > midVal){
+        left = mid + 1;
+      } else{
+        right = mid - 1;
+      }
     }
   }
+}
 
   // main method solely for testing purposes
   public static void main( String[] args )
   {
 
-    OrderedArrayList Franz = new OrderedArrayList();
 
-    // testing linear search
+    OrderedArrayList Franz = new OrderedArrayList();
+    System.out.println(Franz);
+
+    //testing linear search
     for( int i = 0; i < 15; i++ )
-      Franz.addLinear( (int)( 50 * Math.random() ) );
+    Franz.addLinear( (int)( 50 * Math.random() ) );
     System.out.println( Franz );
+
 
     // testing binary search
     Franz = new OrderedArrayList();
+    System.out.println(Franz);
+
     for( int i = 0; i < 15; i++ )
-      Franz.addBinary( (int)( 50 * Math.random() ) );
+    Franz.addBinary( (int)( 50 * Math.random() ) );
     System.out.println( Franz );
     /*-----v-------move-me-down-----------------v--------
-      =====^====================================^=========*/
+    =====^====================================^=========*/
 
   }//end main()
 
